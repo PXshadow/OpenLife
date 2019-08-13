07569e9650c91030f7a16ecffb75d5acafbed10a
Joey <joeyprather8@gmail.com>
2019-08-13 02:51:42 -0700

    remove

--- src/Secret.hx
+++
@@ -1,8 +0,0 @@
-//user account
-class Secret
-{
-    public static var email:String = "";
-    public static var key:String = "";
-    public static var ip:String = "";
-    public static var port:Int = 0;
-}
