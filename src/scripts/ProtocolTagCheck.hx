7004349ef29dfab03aa03f45a553b00616291d77
Joey <joeyprather8@gmail.com>
2020-02-07 23:28:37 -0800

    new protocol

--- src/scripts/ProtocolTagCheck.hx
+++ src/scripts/ProtocolTagCheck.hx
@@ -10,9 +10,22 @@ class ProtocolTagCheck
     {
         new ProtocolTagCheck();
     }
+    var startString = "Possible types include:";
+    var endString = "Client can expect to receive these at any time, and in any order.";
+    var clientTagPath = "./src/client/ClientTag.hx";
+    //more tags
+    var enumTags:Array<String> = [];
+    var names:Array<String> = [];
+    var switchTags:Array<String> = [];
     var string:String;
     public function new()
     {
+        /*if (!FileSystem.exists("./protocol.txt"))
+        {
+            trace("protocol.txt does not exist");
+            return;
+        }
+        string = File.getContent("protocol.txt");*/
         string = haxe.Http.requestUrl("https://raw.githubusercontent.com/jasonrohrer/OneLife/master/server/protocol.txt");
         if (string != File.getContent("./protocol.txt"))
         {
@@ -20,16 +33,11 @@ class ProtocolTagCheck
             File.saveContent("./protocol.txt",string);
         }
         //trace("string " + string);
-        run("./src/client/ClientTag.hx","Possible types include:","Client can expect to receive these at any time, and in any order.");
-        run("./src/server/ServerTag.hx","parameters, and no leading spaces.","KA   ");
+        run();
     }
-    private function run(outputPath:String,startString:String,endString:String)
+    private function run()
     {
-        //more tags
-        var enumTags:Array<String> = [];
-        var names:Array<String> = [];
-        var switchTags:Array<String> = [];
-
+        trace("start protocol tag check");
         var index = string.indexOf(startString) + startString.length;
         if (index <= startString.length)
         {
@@ -52,18 +60,17 @@ class ProtocolTagCheck
         var sub = string.substring(index,index = string.indexOf("@:from",index));
         string = string.substring(index = string.indexOf("case",index),string.indexOf("}",index));
 
-        generate(enumString.split("\n"),enumTags);
-        generate(switchString.split("\n"),switchTags,true);
+        generate(sub.split("\n"),enumTags);
+        generate(string.split("\n"),switchTags,true);
 
         var error:Array<String> = [];
-        var errorBool:Bool = false;
         for (i in 0...tags.length)
         {
+            error = [];
             if (enumTags.indexOf(tags[i]) == -1) error.push("enum");
             if (switchTags.indexOf(tags[i]) == -1) error.push("switch");
             if (error.length > 0)
             {
-                errorBool = true;
                 if (error.length > 1)
                 {
                     trace(names[i] + tags[i] + " | " + error[0] + " and " + error[1] + " not found");
@@ -72,7 +79,6 @@ class ProtocolTagCheck
                 }
             }
         }
-        if (!errorBool) trace("No errors found for tag protocol");
     }
     private function generate(input:Array<String>,output:Array<String>,name:Bool=false)
     {
@@ -84,4 +90,4 @@ class ProtocolTagCheck
             index = output.push(obj.substring(index,obj.indexOf('"',index)));
         }
     }
-}
+} 
