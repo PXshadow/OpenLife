af73432311ae4dce3a4668b4fe5d038b1f9d47e4
PXshadow <joeyprather8@gmail.com>
2020-06-15 22:32:37 -0700

    clean up config

--- app/App.hx
+++ app/App.hx
@@ -3,11 +3,8 @@ package;
 import haxe.ds.IntMap;
 import openlife.data.object.player.PlayerInstance;
 import openlife.engine.Program;
-import sys.io.File;
-import haxe.Json;
 import openlife.data.map.MapInstance;
 import openlife.engine.Engine;
-import sys.FileSystem;
 
 class App extends Engine
 {
@@ -20,45 +17,7 @@ class App extends Engine
         super();
         program = new Program(client);
         var bool:Bool = false;
-        var config:Cred;
-        if (FileSystem.exists("cred"))
-        {
-            Sys.println("Use existing cred config (y)es (n)o");
-            bool = Sys.stdin().readLine() == "n" ? false : true;
-            if (bool)
-            {
-                config = cast Json.parse(File.getContent("cred"));
-                client.ip = config.ip;
-                client.port = config.port;
-                client.legacy = config.legacy;
-                client.email = config.email;
-                client.key = config.key;
-            }
-        }
-        if (!bool)
-        {
-            Sys.println("Legacy authentication (y)es (n)o");
-            client.legacy = Sys.stdin().readLine() == "y" ? true : false;
-            //set credentioals
-            if (!cred())
-            {
-                Sys.println("ip:");
-                string = Sys.stdin().readLine();
-                client.ip = string;
-                Sys.println("port:");
-                string = Sys.stdin().readLine();
-                client.port = Std.parseInt(string);
-                Sys.println("email:");
-                string = Sys.stdin().readLine();
-                client.email = string;
-                Sys.println("key");
-                string = Sys.stdin().readLine();
-                client.key = string;
-                //set config
-                config = {email: client.email, key: client.key,ip: client.ip,port: client.port,legacy: client.legacy};
-                File.saveContent("cred",Json.stringify(config));
-            }
-        }
+        Config.run(client,cred());
         connect(false);
         while (true)
         {
@@ -95,4 +54,3 @@ class App extends Engine
     }
     
 }
-typedef Cred = {legacy:Bool,email:String,key:String,ip:String,port:Int}
