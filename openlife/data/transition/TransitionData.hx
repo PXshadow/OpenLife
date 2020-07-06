2d6ed2b7a71cb734781eec18e89bd8c05417bbb5
PXshadow <joeyprather8@gmail.com>
2020-07-05 22:13:45 -0700

    transistion data

--- openlife/data/transition/TransitionData.hx
+++ openlife/data/transition/TransitionData.hx
@@ -24,36 +24,22 @@ class TransitionData
     public var targetRemains:Bool = false;
     public var decay:Int = 0;
 
-    public function new(name:String,string:String)
+    public function new(fileName:String,string:String)
     {
-        /*
-        const data = dataText.split(' ');
-
-        this.newActorID = data[0] || 0;
-        this.newTargetID = data[1] || 0;
-        this.autoDecaySeconds = data[2] || 0;
-        this.actorMinUseFraction = data[3] || 0;
-        this.targetMinUseFraction = data[4] || 0;
-        this.reverseUseActor = data[5] == '1';
-        this.reverseUseTarget = data[6] == '1';
-        this.move = parseInt(data[7] || 0);
-        this.desiredMoveDist = data[8] || 1;
-        this.noUseActor = data[9] == '1';
-        this.noUseTarget = data[10] == '1';
-
-        this.playerActor = this.actorID == 0;
-        this.tool = this.actorID >= 0 && this.actorID == this.newActorID;
-        this.targetRemains = this.targetID >= 0 && this.targetID == this.newTargetID;
-        */
+        parseFilename(fileName);
+        parseData(string.split(" "));
+    }
+    private function parseFilename(fileName:String)
+    {
         //name
-        var parts = name.split(".")[0].split("_");
+        var parts = fileName.split(".")[0].split("_");
         lastUseActor = (parts[2] == "LA");
         lastUseTarget = (parts[2] == "LT" || parts[2] == "L");
         actorID = Std.parseInt(parts[0]);
         targetID = Std.parseInt(parts[1]);
-        //data
-        var data = string.split(" ");
-        //trace("data " + data);
+    }
+    private function parseData(data:Array<String>)
+    {
         newActorID = Std.parseInt(data[0]);
         newTargetID = Std.parseInt(data[1]);
         autoDecaySeconds = Std.parseInt(data[2]);
@@ -70,5 +56,14 @@ class TransitionData
         playerActor = (actorID == 0);
         tool = (actorID >= 0 && actorID == newActorID);
         targetRemains = (targetID >= 0 && targetID == newTargetID);
+    }
+    public function calculateDecay(seconds:Int):String
+    {
+        if (seconds < 0)
+          return '${-seconds} hours';
+        if (seconds > 0 && seconds % 60 == 0)
+          return '${seconds/60} minutes';
+        if (seconds > 0)
+          return '$seconds seconds';
     }
 }
