bcc94e32fe9bffe6c04ba72ce135ed13a06aec22
PXshadow <joeyprather8@gmail.com>
2020-08-24 00:56:07 -0700

    attempt to fix function, still not giving proper results

--- openlife/data/transition/Recipe.hx
+++ openlife/data/transition/Recipe.hx
@@ -12,16 +12,9 @@ class Recipe
     {
         this.id = id;
     }
-    public function generate(transitions:Array<TransitionData>,categories:Array<Category>,count:Int=0)
+    public function generate(transitions:Array<TransitionData>,categories:Array<Category>,count:Int=0,inital:Bool=true)
     {
-        if (new ObjectData(id).isNatural()) return;
-        /*for (category in categories)
-        {
-            if (category.parentID == id)
-            {
-                trace("category " + category.ids);
-            }
-        }*/
+        if (new ObjectData(id).isNatural() || (a != null && a.a.id == a.id) || (b != null && b.b.id == b.id)) return;
         var trans:Array<TransitionData> = [];
         for (transition in transitions)
         {
@@ -32,28 +25,25 @@ class Recipe
         if (trans.length == 0) return;
         trans.sort(function(a:TransitionData,b:TransitionData)
         {
-            if (a.newActorID == 0 || b.newTargetID == 0) return a.newActorID - b.newTargetID;
-            if (a.targetID == -1 || b.targetID == -1) return b.targetID - a.targetID;
-            return a.targetID - b.targetID;
+            if (a.newActorID <= 0 || b.newTargetID <= 0) return a.newActorID - b.newTargetID;
+            if (a.actorID <= 0 || b.actorID <= 0) return a.actorID - b.actorID;
+            if (a.targetID <= 0 || b.targetID <= 0) return a.targetID - b.targetID;
+            //return a.targetID - b.targetID;
+            return (b.targetID + b.newActorID) - (a.targetID + a.newActorID);
         });
-        if (id == 57)
-        {
-            trace("MILKWEED:");
-            for (tran in trans) trace(tran);
-        }
         var tran = trans.shift();
         Sys.println(tran);
-        if (++count > 10) throw "overflow count to much generation";
-        trace("id " + tran.actorID + " " + tran.targetID);
+        if (count > 20) return;
         if (tran.actorID > 0)
         {
             a = new Recipe(tran.actorID);
-            a.generate(transitions,categories,count);
+            a.generate(transitions,categories,++count,false);
         }
+        if (inital) Sys.println("___________________");
         if (tran.targetID > 0)
         {
             b = new Recipe(tran.targetID);
-            b.generate(transitions,categories,count);
+            b.generate(transitions,categories,++count,false);
         }
     }
     public function depth():Int
