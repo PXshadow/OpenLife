8b095c46d7b364ef90816b1815064df64e3e171d
PXshadow <joeyprather8@gmail.com>
2020-08-26 16:40:27 -0700

    proper depth calculator

--- scripts/Transition.hx
+++ scripts/Transition.hx
@@ -29,14 +29,25 @@ class Transition
         transMap = new Map<Int,Array<NodeData>>();
         for (trans in importer.transitions)
         {
-            add(trans.newActorID,trans);
-            add(trans.newTargetID,trans);
+            if (trans.actorID != trans.newActorID) add(trans.newActorID,trans);
+            if (trans.targetID != trans.newTargetID) add(trans.newTargetID,trans);
         }
         while (true)
         {
             trace("id:");
             var id = Std.parseInt(Sys.stdin().readLine());
-            trace(transMap.get(id));
+            var obj = transMap.get(id);
+            if (obj != null) 
+            {
+                //trace(obj);
+                for (o in obj)
+                {
+                    trace(o);
+                    trace("depth: " + depth(o));
+                }
+            }else{
+                trace("trans null");
+            }
         }
     }
     private inline function get(id:Int):Array<Int>
@@ -45,17 +56,28 @@ class Transition
     }
     private inline function add(id:Int,trans:TransitionData)
     {
+        //expirmental reduction of transitions for production
+        if (id < trans.actorID) return;
+        if (id < trans.targetID) return;
+        //limit only objects
+        if (id <= 0) return;
+        //go through all add add potential list of objects in formula
         for (id in get(id))
         {
             if (id == trans.targetID || id == trans.actorID) continue;
-            if (id <= 0) continue;
             if (!transMap.exists(id)) transMap.set(id,[]);
-            transMap.get(id).unshift({actor: get(trans.actorID),target: get(trans.targetID)});
+            var array = transMap.get(id);
+            var a = get(trans.actorID);
+            var b = get(trans.targetID);
+            array.unshift({actor: a,target: b}); 
         }
     }
-    private inline function depth(node:NodeData)
+    private inline function depth(node:NodeData,value:Int=0):Int
     {
-        
+        var a = transMap.get(node.actor[0]);
+        var b = transMap.get(node.target[0]);
+        Sys.sleep(1/10);
+        return (a == null ? 1 : depth(a[0])) + (b == null ? 1 : depth(b[0])) + value;
     }
 }
 typedef NodeData = {target:Array<Int>,actor:Array<Int>} 
