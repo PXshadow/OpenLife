83d1b91ca4c56b764bd04b4cc77900d9606dd0ce
Arcurus <martin_auer@gmx.net>
2020-11-17 10:36:58 +0100

    added support for storing creator / owner in ObjectHelper

--- openlife/data/object/ObjectHelper.hx
+++ openlife/data/object/ObjectHelper.hx
@@ -1,11 +1,15 @@
 package openlife.data.object;
 
+import openlife.server.GlobalPlayerInstance;
 import openlife.server.Server;
 
 class ObjectHelper {
     public var objectData:ObjectData; 
     public var numberOfUses = 0;
     public var creationTimeInTicks:Int;
+
+    // first one is always creator
+    public var livingOwners:Array<GlobalPlayerInstance> = [];
 
     // to store contained objects in case object is a container
     public var containedObjects:Array<ObjectHelper> = [];
@@ -16,15 +20,20 @@ class ObjectHelper {
     }
     */
 
-    public function new(objectData:ObjectData)
+    public function new(objectData:ObjectData, creator:GlobalPlayerInstance)
     {
         this.objectData = objectData;
+        this.livingOwners[0] = creator;
+
         this.creationTimeInTicks = Server.server.tick;
         this.numberOfUses = objectData.numUses;
         //objectData.numSlots > 0;
-        
-
         //public var useChance:Float = 0;
+    }
+
+    public function getCreator() : GlobalPlayerInstance
+    {
+        return this.livingOwners[0];
     }
 
 }
