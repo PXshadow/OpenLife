9365ccaec1d4dfa872c6255515a902ea5e83b775
Arcurus <martin_auer@gmx.net>
2021-01-01 20:43:05 +0100

    jungle reduces food need up to 33%

--- openlife/server/TimeHelper.hx
+++ openlife/server/TimeHelper.hx
@@ -208,7 +208,11 @@ class TimeHelper
 
         if(c.player.age < ServerSettings.GrownUpAge && c.player.food_store > 0) foodDecay *= ServerSettings.IncreasedFoodNeedForChildren;
 
-        foodDecay *= 1 / (1 + c.player.calculateClothingInsulation()) ; // clothing can reduce food use at max to 1 / 3
+        var biome = WorldMap.worldGetBiomeId(c.player.tx(), c.player.ty());
+
+        var biomeValue = biome == BiomeTag.JUNGLE ? 1.5 : 1; 
+
+        foodDecay /= (biomeValue + c.player.calculateClothingInsulation()) ; // clothing can reduce food use at max to 1 / 3
 
         // if starving to death and there is some health left, reduce food need and heath
         if(c.player.food_store < 0 && c.player.yum_multiplier > 0)
