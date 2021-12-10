0806348646edd9eb9cbd3a42a14109d368a69748
Arcurus <martin_auer@gmx.net>
2021-12-10 16:53:40 +0100

    changed starving speed

--- openlife/settings/ServerSettings.hx
+++ openlife/settings/ServerSettings.hx
@@ -95,8 +95,8 @@ class ServerSettings
     // starving to death
     public static var AgingFactorWhileStarvingToDeath = 0.5; // if starving to death aging is slowed factor XX up to GrownUpAge, otherwise aging is speed up factor XX
     public static var GrownUpAge = 14; // is used for AgingFactorWhileStarvingToDeath and for increase food need for children
-    public static var StarvingToDeathMoveSpeedFactor = 0.5; // reduces speed if stored food is below 0
-    public static var StarvingToDeathMoveSpeedFactorWhileHealthAboveZero = 0.8; // reduces speed if stored food is below 0 and health / yum multiplier > 0
+    public static var StarvingToDeathMoveSpeedFactor = 0.75; // reduces speed if stored food is below 0 // TODO calculate % from max health???
+    public static var StarvingToDeathMoveSpeedFactorWhileHealthAboveZero = 0.9; // reduces speed if stored food is below 0 and health / yum multiplier > 0
     public static var FoodStoreMaxReductionWhileStarvingToDeath = 4; // (5) reduces food store max with factor XX for each food below 0
 
     public static var maxDistanceToBeConsideredAsClose = 200; //20; // only close players are updated with PU and MX and Movement 
