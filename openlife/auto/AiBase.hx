ebe0ba8e59c7ba1d531d8ac9faafa058cd4b2c83
Arcurus <martin_auer@gmx.net>
2022-03-01 11:13:07 +0100

    moved obsolete code

--- openlife/auto/AiBase.hx
+++ openlife/auto/AiBase.hx
@@ -20,31 +20,6 @@ import sys.thread.Thread;
 
 using StringTools;
 using openlife.auto.AiHelper;
-/*
-abstract class AiBase
-{
-    public var seqNum = 1;
-    public var myPlayer(default, default):PlayerInterface;
-
-    public abstract function doTimeStuff(timePassedInSeconds:Float) : Void;
-
-    public abstract function newChild(child:PlayerInterface) : Void;
-    public abstract function say(player:PlayerInterface, curse:Bool, text:String) : Void;
-    public abstract function finishedMovement() : Void;
-    public abstract function newBorn() : Void;
-    public abstract function emote(player:PlayerInstance, index:Int) : Void;
-	public abstract function playerUpdate(player:PlayerInstance) : Void;
-	public abstract function mapUpdate(targetX:Int, targetY:Int, isAnimal:Bool = false) : Void;
-	public abstract function playerMove(player:PlayerInstance, targetX:Int, targetY:Int) : Void;
-
-    public abstract function isObjectNotReachable(tx:Int, ty:Int):Bool;
-    public abstract function addNotReachableObject(obj:ObjectHelper, time:Float = 90) : Void;
-    public abstract function addNotReachable(tx:Int, ty:Int, time:Float = 90) : Void;
-    public abstract function isObjectWithHostilePath(tx:Int, ty:Int):Bool; 
-
-    public abstract function resetTargets() : Void;
-}*/
-
 
 abstract class AiBase
 {
@@ -1668,4 +1643,30 @@ abstract class AiBase
 		} 
 		
 		if(itemToCraft.transActor != null) trace('ai: craft: steps: $bestSteps Distance: $bestDistance bestActor: ${itemToCraft.transActor.description} / target: ${itemToCraft.transTarget.id} ${itemToCraft.transTarget.description} ' + bestTrans.getDesciption());
+}*/
+
+
+/*
+abstract class AiBase
+{
+    public var seqNum = 1;
+    public var myPlayer(default, default):PlayerInterface;
+
+    public abstract function doTimeStuff(timePassedInSeconds:Float) : Void;
+
+    public abstract function newChild(child:PlayerInterface) : Void;
+    public abstract function say(player:PlayerInterface, curse:Bool, text:String) : Void;
+    public abstract function finishedMovement() : Void;
+    public abstract function newBorn() : Void;
+    public abstract function emote(player:PlayerInstance, index:Int) : Void;
+	public abstract function playerUpdate(player:PlayerInstance) : Void;
+	public abstract function mapUpdate(targetX:Int, targetY:Int, isAnimal:Bool = false) : Void;
+	public abstract function playerMove(player:PlayerInstance, targetX:Int, targetY:Int) : Void;
+
+    public abstract function isObjectNotReachable(tx:Int, ty:Int):Bool;
+    public abstract function addNotReachableObject(obj:ObjectHelper, time:Float = 90) : Void;
+    public abstract function addNotReachable(tx:Int, ty:Int, time:Float = 90) : Void;
+    public abstract function isObjectWithHostilePath(tx:Int, ty:Int):Bool; 
+
+    public abstract function resetTargets() : Void;
 }*/
