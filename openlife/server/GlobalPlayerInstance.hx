9f10edac8f0b36f553f6f14e8e63c9c6df87bbd7
Arcurus <martin_auer@gmx.net>
2022-03-02 10:12:41 +0100

    full recovered exhaustion on birth

--- openlife/server/GlobalPlayerInstance.hx
+++ openlife/server/GlobalPlayerInstance.hx
@@ -757,6 +757,7 @@ class GlobalPlayerInstance extends PlayerInstance implements PlayerInterface imp
 
 		food_store_max = calculateFoodStoreMax();
 		food_store = food_store_max / 2;
+		exhaustion = -food_store_max;
 		yum_multiplier = this.account.totalScore * ServerSettings.BirthPrestigeFactor;
 		yum_multiplier = Math.max(yum_multiplier, (medianPrestige / 30) * trueAge);
 
