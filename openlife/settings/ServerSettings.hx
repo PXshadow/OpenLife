158db6f1758ec57092895b9c6148f599e22a09a0
Arcurus <martin_auer@gmx.net>
2022-03-03 17:28:08 +0100

    more snakes

--- openlife/settings/ServerSettings.hx
+++ openlife/settings/ServerSettings.hx
@@ -373,8 +373,10 @@ class ServerSettings {
 		ObjectData.getObjectData(418).biomes.push(BiomeTag.YELLOW); // Happy Wolfs now also in Yellow biome :)
 		//ObjectData.getObjectData(418).biomes.push(BiomeTag.GREEN); // Happy Wolfs now also in Green biome :)
 		ObjectData.getObjectData(418).biomes.push(BiomeTag.SNOW); // Happy Wolfs now also in Snow biome :)
-		ObjectData.getObjectData(418).mapChance *= 1.1; // more Happy Wolfs
+		ObjectData.getObjectData(418).mapChance *= 1.2; // more Happy Wolfs
 		ObjectData.getObjectData(418).speedMult *= 1.5; // Boost Wolfs even more :)
+
+		ObjectData.getObjectData(764).mapChance *= 1.5; // more snakes needs the world!
 
 		ObjectData.getObjectData(411).speedMult = 0.8; // Fertile Soil Reduced carring speed
 		ObjectData.getObjectData(345).speedMult = 0.8; // Butt Log
