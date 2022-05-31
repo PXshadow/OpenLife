911c3ee8733d8954131884adbb66a63659f0e993
Joey <9813078+PXshadow@users.noreply.github.com>
2022-05-31 16:01:18 +0200

    Client animation: releative rot offset, and fade

--- openlife/client/Object.hx
+++ openlife/client/Object.hx
@@ -16,9 +16,9 @@ class Object {
 		this.x = x;
 		this.y = y;
 		this.data = data;
-		/*try {
+		try {
 			anim = new AnimationData(data.id);
-		} catch (_) {}*/
+		} catch (_) {}
 	}
 
 	public function play() {}
@@ -35,10 +35,45 @@ class Object {
 			final sprite = sprites[i];
 			sprite.x = x + data.spriteArray[i].x;
 			sprite.y = y - data.spriteArray[i].y;
-			sprite.rotation = data.spriteArray[i].rot * Math.PI * 2;
+			sprite.rotation = -data.spriteArray[i].rot * 2 * Math.PI;
 			sprite.x += getOscOffset(inFrameTime, param.offsetX, param.xOscPerSec, param.xAmp, param.xPhase);
 			sprite.y += -getOscOffset(inFrameTime, param.offsetY, param.yOscPerSec, param.yAmp, param.yPhase);
-			sprite.rotation += getOscOffset(inFrameTime, 0, param.rockOscPerSec, param.rockAmp, param.rockPhase) * Math.PI * 2;
+			sprite.rotation += getOscOffset(inFrameTime, 0, param.rockOscPerSec, param.rockAmp, param.rockPhase) * 2 * Math.PI;
+			final totalRotOffset = param.rotPerSec * inFrameTime + param.rotPhase;
+
+			final sinVal = getOscOffset(inFrameTime, 0, param.fadeOscPerSec, 1, param.fadePhase + 0.25);
+
+			final hardness = param.fadeHardness;
+			var hardVersion = 0.0;
+
+			if (hardness == 1) {
+				hardVersion = sinVal > 0 ? 1 : -1;
+			} else {
+				var absSinVal = Math.abs(sinVal);
+				hardVersion = absSinVal != 0 ? (sinVal / absSinVal) * Math.pow(absSinVal, 1 / (hardness * 10 + 1)) : 0;
+			}
+
+			final fade = (param.fadeMax - param.fadeMin) * (0.5 * hardVersion + 0.5) + param.fadeMin;
+
+			sprite.alpha = fade;
+
+			// relative to 0 on circle
+			var relativeRotOffset = totalRotOffset - Math.floor(totalRotOffset);
+
+			// make positive
+			if (relativeRotOffset < 0) {
+				relativeRotOffset += 1;
+			}
+
+			// to take average of two rotations
+			// rotate them both so that one is at 0.5,
+			// take average, and then rotate them back
+			// This ensures that we always move through closest average
+			// point on circle (shortest path along circle).
+
+			var offset = 0.5 - relativeRotOffset;
+
+			sprite.rotation += relativeRotOffset * Math.PI * 2;
 		}
 		for (i in 0...record.params.length) {
 			if (data.spriteArray[i].parent != -1) continue;
@@ -81,9 +116,4 @@ class Object {
 
 	private inline function getOscOffset(inFrameTime:Float, inOffset:Float, inOscPerSec:Float, inAmp:Float, inPhase:Float)
 		return inOffset + inAmp * Math.sin((inFrameTime * inOscPerSec + inPhase) * 2 * Math.PI);
-
-	private inline function phase(x:Float):Float {
-		if (x > 0.75) return x - 1;
-		return (x * 2 - 1) * -2;
-	}
 }
