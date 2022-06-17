4f94715c21c04a1448afe9be4ef48b65363c7e37
Arcurus <martin_auer@gmx.net>
2022-06-17 14:31:57 +0200

    TemperatureSpeedImpact 0.9 ==> 1

--- openlife/settings/ServerSettings.hx
+++ openlife/settings/ServerSettings.hx
@@ -263,7 +263,7 @@ class ServerSettings {
 	public static var TemperatureLovedBiomeFactor:Float = 1; 
 	public static var TemperatureMaxLovedBiomeImpact:Float = 0.1; // max 0.1 better in loved biome (right color or borh parents right color)
 	public static var TemperatureImpactBelow:Float = 0.6; // take damage and display emote if temperature is below or above X from normal
-	public static var TemperatureSpeedImpact:Float = 0.9; // speed * X: double impact if extreme temperature
+	public static var TemperatureSpeedImpact:Float = 1.0; // 0.0 // speed * X: double impact if extreme temperature
 	
 	public static var TemperatureShiftForBlack:Float = 0.1; // ideal temperature = 0.6
 	public static var TemperatureShiftForBrown:Float = 0.05;
