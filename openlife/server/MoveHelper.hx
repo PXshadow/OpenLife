47c2d30b9f1f3724ff630fada11152da2026e43f
Arcurus <martin_auer@gmx.net>
2022-06-17 14:29:03 +0200

    better speed for horses on road and in bad biome

--- openlife/server/MoveHelper.hx
+++ openlife/server/MoveHelper.hx
@@ -127,11 +127,11 @@ class MoveHelper {
 		// DO speed held objects
 		var speedModHeldObj = p.heldObject.objectData.speedMult;
 
-		if (biomeSpeed < 0.999 && speedModHeldObj > 1) // horses and cars are bad in bad biome
+		if (fullPathHasRoad == false && biomeSpeed < 0.999 && speedModHeldObj > 1) // horses and cars are bad in bad biome
 		{
-			if (speedModHeldObj > 2.50) speedModHeldObj = 0.8; // super speedy stuff like cars
-			else if (speedModHeldObj > 1.8) speedModHeldObj = 0.9; // for example horse
-			else if (speedModHeldObj > 1.2) speedModHeldObj = 0.8; // for example horse cart
+			if (speedModHeldObj > 2.50) speedModHeldObj = 0.9; // super speedy stuff like cars
+			else if (speedModHeldObj > 1.8) speedModHeldObj = 1; // for example horse
+			else if (speedModHeldObj > 1.2) speedModHeldObj = 0.9; // for example horse cart
 
 			if (ServerSettings.DebugSpeed)
 				trace('Speed: New ${p.heldObject.objectData.description} speed in bad biome: ${p.heldObject.objectData.speedMult} --> $speedModHeldObj');
