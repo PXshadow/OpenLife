b3fa2bfaa6684afb506589e9bcbbd725c3ba8650
Arcurus <martin_auer@gmx.net>
2022-06-21 13:58:01 +0200

    Change time: X24  Stone Floor ==> Ancient

--- openlife/settings/ServerSettings.hx
+++ openlife/settings/ServerSettings.hx
@@ -831,8 +831,10 @@ class ServerSettings {
 		ObjectData.getObjectData(30).lastUseObject = 279; // Wild Gooseberry Bush ==> Empty Wild Gooseberry Bush
 		ObjectData.getObjectData(279).undoLastUseObject = 30; // Empty Wild Gooseberry Bush ==> Wild Gooseberry Bush
 
-		var trans = transtions.getTransition(-1, 887); // Stone Wall
-		trans.autoDecaySeconds = -24 * 10; // default: -10
+		var trans = transtions.getTransition(-1, 887); // Stone Wall ==> Ancient
+		trans.autoDecaySeconds = -24 * 10; // default: -10 // TODO implement decay for Ancient
+		var trans = transtions.getTransition(-1, 884); // Stone Floor ==> Ancient
+		trans.autoDecaySeconds = -24 * 10; // default: -10 // TODO implement for floors
 
 		var trans = transtions.getTransition(-1, 750); // Bloody Knife
 		trans.autoDecaySeconds = 50;
