81aad87492fe5ad6cfbab98936ff8cc5891c3ab7
Arcurus <martin_auer@gmx.net>
2022-06-28 14:52:08 +0200

    Fobid to undig domestic bush

--- openlife/settings/ServerSettings.hx
+++ openlife/settings/ServerSettings.hx
@@ -1779,6 +1779,9 @@ class ServerSettings {
 		var trans = transtions.getTransition(185, 346); // Skewered Rabbit + Large Slow Fire
 		trans.aiShouldIgnore = true;
 
+		var trans = transtions.getTransition(107, 392); // Stakes + Languishing Domestic Gooseberry Bush
+		trans.aiShouldIgnore = true;
+
 		//var trans = transtions.getTransition(235, -1); // 235 Clay Bowl
 		//trace('DEBUG: ${trans.getDesciption()}');
 
