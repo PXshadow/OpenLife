74cce2deba3704437a849c104d5b5bc080a5d7c6
Arcurus <martin_auer@gmx.net>
2022-06-29 18:19:49 +0200

    allow Rabit to decay

--- openlife/server/TimeHelper.hx
+++ openlife/server/TimeHelper.hx
@@ -1613,8 +1613,8 @@ class TimeHelper {
 		//if (containsSomething && (floorId > 0 || objId != 292)) return; // TODO 292 Basket ==> Allow all containers
 		if (floorId > 0 && (objData.isWall() == false || containsSomething)) return; // TODO Decay Allow containers in walls
 
-		// only allow object with time transition to decay if it takes longer then one hour 
-		if (objectHelper != null && objectHelper.timeToChange > 0 && objectHelper.timeToChange < 3600) return; 
+		// only allow object with time transition to decay if it takes longer then one hour // 161 Rabbit to unstuck them from the corner
+		if (objectHelper != null && objectHelper.timeToChange > 0 && objectHelper.timeToChange < 3600 && countAs != 161) return; 
 
 		var objData = ObjectData.getObjectData(objId);
 
