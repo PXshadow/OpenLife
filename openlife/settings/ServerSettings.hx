b3130cdcf5c44de18a6954bf898e2e4defb30613
Arcurus <martin_auer@gmx.net>
2022-07-10 19:47:58 +0200

    little bit more banana

--- openlife/settings/ServerSettings.hx
+++ openlife/settings/ServerSettings.hx
@@ -1655,7 +1655,7 @@ class ServerSettings {
 		// let get bana back!
 		trans = new TransitionData(-1, 2142, 0, 2142); // Banana Plant
 		trans.reverseUseTarget = true;
-		trans.autoDecaySeconds = -4;
+		trans.autoDecaySeconds = -2;
 		transtions.addTransition("PatchTransitions: ", trans);
 
 		trans = new TransitionData(-1, 2145, 0, 2142); // Empty Banana Plant --> Banana Plant
