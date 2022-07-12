7c8725c267d2fadc4d556c370ccf4bc3f9302d1a
Arcurus <martin_auer@gmx.net>
2022-07-12 20:51:02 +0200

    Litte bit more Ais

--- openlife/settings/ServerSettings.hx
+++ openlife/settings/ServerSettings.hx
@@ -327,7 +327,7 @@ class ServerSettings {
 	public static var PrestigeCostPerDamageForWomenWithoutWeapon:Float = 0.5; //0.25
 
 	// AI
-	public static var NumberOfAis:Int = 20;
+	public static var NumberOfAis:Int = 30;
 	public static var NumberOfAiPx:Int = 0;
 	public static var AiReactionTime:Float = 0.5; // 0.5;
 	public static var TimeToAiRebirthPerYear:Float = 10; // X seconds per not lived year = 60 - death age
