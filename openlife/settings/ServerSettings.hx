703f4af1f39b57ce646e5f7257a328e5e6b4285a
Arcurus <martin_auer@gmx.net>
2022-07-13 21:49:09 +0200

    AI: forbid: 0 + Sliced Bread

--- openlife/settings/ServerSettings.hx
+++ openlife/settings/ServerSettings.hx
@@ -2168,6 +2168,9 @@ class ServerSettings {
 		var trans = transtions.getTransition(252, 291,true); // Bowl of Dough + Flat Rock
 		trans.aiShouldIgnore = true;
 
+		var trans = transtions.getTransition(0, 1471); // 0 + Sliced Bread
+		trans.aiShouldIgnore = true; // they make a mess to get the plate
+
 		// forbid burning stuff
 		var trans = transtions.getTransition(516, 82); // Skewered Goose + Fire
 		trans.aiShouldIgnore = true;
