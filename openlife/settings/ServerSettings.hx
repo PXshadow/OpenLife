c94bb3f2f7cad01fd044b62f88b19e32188c836b
Arcurus <martin_auer@gmx.net>
2022-12-01 11:44:55 +0100

    Nerve Wild foods

--- openlife/settings/ServerSettings.hx
+++ openlife/settings/ServerSettings.hx
@@ -1017,14 +1017,18 @@ class ServerSettings {
 		ObjectData.getObjectData(861).speedMult = 0.85; // // Old Hand Cart
 		ObjectData.getObjectData(2172).speedMult = 0.9; // Hand Cart with Tires
 
-		// nerve food
-		ObjectData.getObjectData(2143).foodValue = 6; // banana // origional 7
-		ObjectData.getObjectData(31).foodValue = 4; // Gooseberry // origional 3
+		// nerve food		
+		ObjectData.getObjectData(768).foodValue = 4; // Cactus Fruit 768 // origional 8
+		ObjectData.getObjectData(2143).foodValue = 5; // banana // origional 7
+		ObjectData.getObjectData(31).foodValue = 3; // Gooseberry // origional 3
+		ObjectData.getObjectData(253).foodValue = 3; // BOWL OF GOOSEBERRIES 253 // origional 3
 		ObjectData.getObjectData(2855).foodValue = 4; // Onion // origional 5
-		ObjectData.getObjectData(808).foodValue = 4; // Wild Onion // origional 4
-		ObjectData.getObjectData(807).foodValue = 5; // Burdock Rootl 7
-		//ObjectData.getObjectData(40).foodValue = 5; // Wild Carrot // origional 5
-		//ObjectData.getObjectData(402).foodValue = 5; // Carrot // origional 5
+		ObjectData.getObjectData(808).foodValue = 3; // Wild Onion // origional 4
+		ObjectData.getObjectData(807).foodValue = 4; // Burdock Rootl 7
+		ObjectData.getObjectData(40).foodValue = 4; // Wild Carrot // origional 5
+		ObjectData.getObjectData(402).foodValue = 4; // Carrot // origional 5
+		ObjectData.getObjectData(4252).foodValue = 3; // WILD GARLIC 4252 // origional 4
+		//ObjectData.getObjectData(837).foodValue = 1; // PSILOCYBE MUSHROOM 837 // origional 1
 
 		// boost hunted food
 		ObjectData.getObjectData(197).foodValue = 15; // Cooked Rabbit 10 --> 15
