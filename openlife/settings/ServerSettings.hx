bd877544db1e11255a0e8bcad740885029e7d33c
Arcurus <martin_auer@gmx.net>
2023-05-24 13:33:27 +0200

    Kill Sheep only if 5 or more (old 3)

--- openlife/settings/ServerSettings.hx
+++ openlife/settings/ServerSettings.hx
@@ -3435,16 +3435,16 @@ class ServerSettings {
 		// Bow and Arrow 152 + Mouflon 531
 		LimitTransitionIfMinNotReached(152, 531, 531, 3); // Allow to kill Mouflon if needed are reached
 		// Knife 560 +  Domestic Sheep 575
-		LimitTransitionIfMinNotReached(560, 575, 575, 3); // Allow to kill Sheep if more than 3
+		LimitTransitionIfMinNotReached(560, 575, 575, 5); // Allow to kill Sheep if enough
 		// Knife 560 +  Shorn Domestic Sheep 576
-		LimitTransitionIfMinNotReached(560, 576, 576, 2); // Allow to kill Sheep if more than 2
+		LimitTransitionIfMinNotReached(560, 576, 576, 3); // Allow to kill Sheep if enough
 
 		// Knife 560 + Domestic Cow 1458
-		LimitTransitionIfMinNotReached(560, 1458, 1458, 3); // Allow to kill Cow if more than 3
+		LimitTransitionIfMinNotReached(560, 1458, 1458, 4); // Allow to kill Cow if enough
 		// War Sword 3047 + Domestic Cow 1458
-		LimitTransitionIfMinNotReached(3047, 1458, 1458, 3); // Allow to kill Cow if more than 3
+		LimitTransitionIfMinNotReached(3047, 1458, 1458, 4); // Allow to kill Cow if enough
 		// Mango Leaf 1878 + Domestic Cow 1458
-		LimitTransitionIfMinNotReached(1878, 1458, 1458, 2); // Allow to kill Cow if more than 2
+		LimitTransitionIfMinNotReached(1878, 1458, 1458, 2); // Allow to kill Cow if enough
 	}
 
 	private static function LimitTransitionIfMinNotReached(actorId:Int, targetId:Int, id:Int, min:Int = 3) {
