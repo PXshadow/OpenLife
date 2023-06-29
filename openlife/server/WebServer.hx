1614375289874d6dea669e6bd944e5a14b5f6a11
Arcurus <martin_auer@gmx.net>
2023-06-29 16:03:53 +0200

    Added first WebsServer for Open Life Reborn to display current player count: openlifereborn.com

--- openlife/server/WebServer.hx
+++ openlife/server/WebServer.hx
@@ -6,6 +6,7 @@ import sys.net.Socket;
 
 class WebServer {
 	public var listenCount:Int = 10;
+	public var startText:String = null;
 
 	public static function Start() {
 		var webServer = new WebServer();
@@ -35,6 +36,8 @@ class WebServer {
 
 		while (true) {
 			var socket = serverSocket.accept();
+			socket.setBlocking(false);
+			socket.setFastSend(true);
 			handleRequest(socket);
 		}
 	}
@@ -44,13 +47,25 @@ class WebServer {
 		server.bind("127.0.0.1", 8080);
 		server.listen(handleRequest);
 	}*/
-	private static function handleRequest(socket:Socket) {
+	private function handleRequest(socket:Socket) {
 		trace('received request!');
 
-		var text = 'Welcome to Open Life Reborn!';
-		var message = 'HTTP/1.1 200 OK\r\nContent-Length: ${text.length}\r\n\r\n$text';
-		// socket.output.write(response);
-		socket.output.writeString(message);
+		if (startText == null) startText = createStartText();
+
+		socket.output.writeString(startText);
 		socket.close();
+		startText = createStartText(); // TODO count once in a while
+	}
+
+	private static function createStartText() {
+		var text = 'Welcome to Open Life Reborn!\n';
+
+		GlobalPlayerInstance.AcquireMutex();
+		var count = Connection.CountHumans();
+		GlobalPlayerInstance.ReleaseMutex();
+
+		text += 'Currently Playing: ${count}';
+		var message = 'HTTP/1.1 200 OK\r\nContent-Length: ${text.length}\r\n\r\n$text';
+		return message;
 	}
 }
